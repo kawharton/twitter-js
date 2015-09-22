6e5062d2a68142923d9b81e5ea9f5a04b2e6d6da
oceanwaves19 <Khadija@Khadijas-MacBook-Pro.local>
2015-09-22 18:30:30 -0400

    added swig to template engine

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var swig = require('swig');
 
 var app = express();
 
@@ -6,6 +7,12 @@ app.use(function (req, res, next){
 	console.log(req.method + " " + req.originalUrl);
 	next();
 })
+
+app.use(function (req, res, next){
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+res.render( 'index', {title: 'Hall of Fame', people: people} );
+})
+
 
 app.get('/', function(req, res) {
 	res.send('Welcome!');
@@ -15,7 +22,27 @@ app.get('/news', function(req, res) {
 	res.send('Welcome to news!');
 })
 
+app.engine('html', swig.renderFile);
 
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({ cache: false });
+
+var data = {
+    title: 'An Example',
+    people: [{
+        name: 'Gandalf',
+    }, {
+        name: 'Frodo'
+    }, {
+        name: 'Hermione'
+    }]
+};
+
+swig.renderFile(__dirname + '/views/index.html', data, function (err, output) {
+    console.log(output);
+});
 
 
 
