9da8c4cffc818ffb14ea722f69d784a73ff0ed74
oceanwaves19 <Khadija@Khadijas-MacBook-Pro.local>
2015-09-22 17:47:33 -0400

    configured express

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ var express = require('express');
 
 var app = express();
 
+app.use(function (req, res, next){
+	console.log(req.method + " " + req.originalUrl);
+	next();
+})
+
 app.get('/', function(req, res) {
 	res.send('Welcome!');
 })
 
+app.get('/news', function(req, res) {
+	res.send('Welcome to news!');
+})
 
 
 
