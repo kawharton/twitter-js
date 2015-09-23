195ad8e1e250809e0077c0763429bf22e32bff94
oceanwaves19 <Khadija@Khadijas-MacBook-Pro.local>
2015-09-23 11:06:45 -0400

    routing and views- live test

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 var express = require('express');
 var swig = require('swig');
+var routes = require('./routes/');
+
 
 var app = express();
 
+//middleware
 app.use(function (req, res, next){
 	console.log(req.method + " " + req.originalUrl);
 	next();
 })
 
-app.use(function (req, res, next){
-	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-res.render( 'index', {title: 'Hall of Fame', people: people} );
-})
+
+app.use(express.static(__dirname + '/public'));
+app.use('/', routes);
 
 
-app.get('/', function(req, res) {
-	res.send('Welcome!');
-})
 
-app.get('/news', function(req, res) {
-	res.send('Welcome to news!');
-})
+
+// app.use(function (req, res, next){
+// 	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// res.render( 'index', {title: 'Hall of Fame', people: people} );
+// })
+
+
+// app.get('/', function(req, res) {
+// 	res.send('Welcome!');
+// })
+
+// app.get('/news', function(req, res) {
+// 	res.send('Welcome to news!');
+// })
 
 app.engine('html', swig.renderFile);
 
@@ -29,20 +39,11 @@ app.set('views', __dirname + '/views');
 
 swig.setDefaults({ cache: false });
 
-var data = {
-    title: 'An Example',
-    people: [{
-        name: 'Gandalf',
-    }, {
-        name: 'Frodo'
-    }, {
-        name: 'Hermione'
-    }]
-};
 
-swig.renderFile(__dirname + '/views/index.html', data, function (err, output) {
-    console.log(output);
-});
+
+// swig.renderFile(__dirname + '/views/index.html', data, function (err, output) {
+//     console.log(output);
+// });
 
 
 
